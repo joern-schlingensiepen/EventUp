8b8c1faa4de43a4209b838986f79fc3bff591903
Jessica Katherine Sastre Lozada <jes4553@thi.de>
2023-12-09 13:25:57 +0100

    status 09.12.23

--- EventUpWebApp/Models/EventViewModel.cs
+++ EventUpWebApp/Models/EventViewModel.cs
@@ -22,7 +22,11 @@ namespace EventUpWebApp.Models
         public string Name { get; set; }
         public string City { get; set; }
         public string Address { get; set; }
+
+        [DisplayName("Number of guest")]
         public int NumberOfGuest { get; set; }
+
+        [DisplayName("Budget [Euro]")]
         public Nullable<double> Budget { get; set; }
         public string Typ_Event { get; set; }
         public List<SelectListItem> TypEventOptions { get; set; }
