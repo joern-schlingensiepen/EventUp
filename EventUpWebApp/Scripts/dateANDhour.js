a0942c5f31cff51d9e1f812c5d67ed3b2891afc6
Jessica Katherine Sastre Lozada <jes4553@thi.de>
2024-01-23 17:35:28 +0100

    start_DateTime since current date, ok

--- EventUpWebApp/Scripts/dateANDhour.js
+++ EventUpWebApp/Scripts/dateANDhour.js
@@ -1,21 +1,25 @@
 ﻿$(function () {
-    // Obtener la fecha actual en formato UTC
+    // current date
     var currentDate = new Date();
     var minStartDate = formatDate(currentDate);
 
-    // Establecer la fecha mínima para el campo de fecha de inicio
     $("#Start_DateTime").attr("min", minStartDate);
 
-    // Manejar el cambio en la fecha de inicio
+    
     $("#Start_DateTime").change(function () {
-        // Obtener el valor de la fecha de inicio
+        
         var startDate = new Date($(this).val());
 
-        // Establecer la fecha mínima para el campo de fecha de finalización
+        
+        if (startDate < currentDate) {
+            $(this).val(minStartDate);
+        }
+
+        // End_Date Time after Start_DateTime
         $("#End_DateTime").attr("min", formatDate(startDate));
     });
 
-    // Función para formatear la fecha como se requiere para el atributo "min"
+    // format the date as required for the attribute "min".
     function formatDate(date) {
         var year = date.getUTCFullYear();
         var month = ('0' + (date.getUTCMonth() + 1)).slice(-2);
@@ -26,3 +30,4 @@
         return year + '-' + month + '-' + day + 'T' + hours + ':' + minutes;
     }
 });
+
